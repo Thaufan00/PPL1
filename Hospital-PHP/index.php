aaa57a2faa3eeeb8665bee3d3b414ba04fc286b9
Thaufan00 <thaufan2016@gmail.com>
2023-10-14 05:35:44 +0700

    PPL1 tugas kelompok

---
+++ Hospital-PHP/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required Meta Tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Page Title -->
+    <title>Sistem Manajemen Rumah Sakit</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="assets/images/logo/logo.jpg" type="image/x-icon">
+
+    <!-- CSS Files -->
+    <link rel="stylesheet" href="assets/css/animate-3.7.0.css">
+    <link rel="stylesheet" href="assets/css/font-awesome-4.7.0.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-4.1.3.min.css">
+    <link rel="stylesheet" href="assets/css/owl-carousel.min.css">
+    <link rel="stylesheet" href="assets/css/jquery.datetimepicker.min.css">
+    <link rel="stylesheet" href="assets/css/linearicons.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <!-- Preloader Starts -->
+    <div class="preloader">
+        <div class="spinner"></div>
+    </div>
+    <!-- Preloader End -->
+
+    <!-- Header Area Starts -->
+    <header class="header-area">
+        <div id="header" id="home">
+            <div class="container">
+                <div class="row align-items-center justify-content-between d-flex">
+                <div id="logo">
+                    <a href="index.php"></a>
+                </div>
+                <nav id="nav-menu-container">
+                    <ul class="nav-menu">
+                        <li class="menu-active"><a href="index.php">Home</a></li>
+                        <li><a href="backend/doc/index.php">Doctor's Login</a></li>
+                        <li><a href="backend/admin/index.php">Administrator Login</a></li>
+                    </ul>
+                </nav><!-- #nav-menu-container -->		    		
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Header Area End -->
+
+    <!-- Banner Area Starts -->
+    <section class="banner-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <h4>Merawat  anda tentu kehidupan sangat menyenangkan</h4>
+                    
+                    <p>Rumah sakit dianugerahi sebagai salah satu Sistem Manajemen Rumah Sakit Terbaik, yang dapat mengintegrasikan semua sistem dan proses, istem informasi cerdas untuk memperoleh efisiensi operasional dan membantu rumah sakit dalam proses pengambilan keputusan melalui MIS dan Analitik.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+   
+
+
+    <!-- Javascript -->
+    <script src="assets/js/vendor/jquery-2.2.4.min.js"></script>
+	<script src="assets/js/vendor/bootstrap-4.1.3.min.js"></script>
+    <script src="assets/js/vendor/wow.min.js"></script>
+    <script src="assets/js/vendor/owl-carousel.min.js"></script>
+    <script src="assets/js/vendor/jquery.datetimepicker.full.min.js"></script>
+    <script src="assets/js/vendor/jquery.nice-select.min.js"></script>
+    <script src="assets/js/vendor/superfish.min.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+</html>
